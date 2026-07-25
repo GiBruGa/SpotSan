5d5a5b3ab98603e9c54b2718b9d1b294c16ae8d4
Gilles BRUN GAUTIER <Gilles.BrunGautier@UrBizia-aps.com>
2026-07-25 12:35:25 +0200

    Bump service worker cache name so v3.4 assets actually reach already-installed devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'urbizia-plansan-mobile-v1';
+const CACHE_NAME = 'urbizia-plansan-mobile-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
 self.addEventListener('install', (e) => {
